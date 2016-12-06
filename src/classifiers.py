4714ee2f8729d3cb48e5ffbc534cd20bea7aaec3
Charles Bine <charles.bine@gmail.com>
2016-12-06 11:30:07 -0500

    Cleanup of

--- src/classifiers.py
+++ src/classifiers.py
@@ -3,21 +3,31 @@ import classifiers_tf as cl_tf
 # import classifiers_theano as cl_theano
 
 class Classifier:
-  'Mainly TensorFlow code to classify data'
-  def __init__(self, type, framework):
-    self.type = type
-    self.framework = framework
+    """
+    Mainly TensorFlow code to classify data
+    """
+    def __init__(self, type, framework):
+        self.type = type
+        self.framework = framework
 
-  def classify(self, dataset, percentage_train, percentage_test, batch_size, learning_rate):
+        def classify(self, dataset, percentage_train, percentage_test, batch_size, learning_rate):
 
-    if self.type == "softmax":
-      if self.framework == "tf":
-        return cl_tf.classify_with_softmax_nn(dataset, percentage_train, percentage_test, batch_size, learning_rate)
-      elif self.framework == "torch":
-        raise Exception('Torch implementation not done yet')
+            if self.type == "softmax":
+                if self.framework == "tf":
+                    return cl_tf.classify_with_softmax_nn(dataset, percentage_train, percentage_test, batch_size, learning_rate)
+                elif self.framework == "torch":
+                    raise NotImplemented('Torch implementation not done yet')
+                else:
+                    raise NameError("Framework must be 'tf' or 'torch'")
 
-    elif self.type == "cnn":
-      if self.framework == "tf":
-        return cl_tf.classify_with_conv_nn(dataset, percentage_train, percentage_test, batch_size, learning_rate)
-      elif self.framework == "torch":
-        raise Exception('Torch implementation not done yet')
+
+            elif self.type == "cnn":
+                if self.framework == "tf":
+                    return cl_tf.classify_with_conv_nn(dataset, percentage_train, percentage_test, batch_size, learning_rate)
+                elif self.framework == "torch":
+                    raise NotImplemented('Torch implementation not done yet')
+                else:
+                    raise NameError("Framework must be 'tf' or 'torch'")
+
+            else:
+                raise NameError("Type must be 'softmax' or 'cnn'")
