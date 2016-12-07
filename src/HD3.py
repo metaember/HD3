fad4106338ada171f07d2bd4e8ab3980b4cbe90a
Charles Bine <charles.bine@gmail.com>
2016-12-07 17:32:00 -0500

    bugfix for CLI

--- src/HD3.py
+++ src/HD3.py
@@ -11,19 +11,22 @@ args = parser.parse_args()
 
 # constants
 MODES = ("softmax", "cnn")
-PREPROC = tuple()
+PREPROC = (None,)
 DATASETS = ("mnist",)
+
+if args.dataset == None:
+    args.dataset = "mnist"
 
 # main UI switch
 if args.mode not in MODES:
     raise NameError("{} is not an available mode. Try {}".format(args.mode, MODES))
 elif args.preprocess not in PREPROC:
     raise NameError("{} is not an available preprocessing option. Try {}".format(args.preprocess, PREPROC))
-elif args.dataset.tolower() not in DATASETS:
+elif args.dataset not in DATASETS:
     raise NameError("{} is not an available dataset. Try {}".format(args.dataset, DATASETS))
 else:
     from test_full_pipeline import *
     print("Starting. Mode = {}, preprocessing = {}, dataset = {}, verbose = {}".format(
                 args.mode, args.preprocess, args.dataset, args.verbose))
     print()
-    run_test(args.mode, verbose=args.verbose)
+    run_test(args.mode, preproc = args.preprocess, verbose=args.verbose)
